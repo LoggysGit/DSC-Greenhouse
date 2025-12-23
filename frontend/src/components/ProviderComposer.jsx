abc1cec74dff440af1b216cbdac50cad4b00d2c3
Loggys <albertmetsler3008@gmail.com>
2025-12-23 13:11:42 +0500

    Deploy from dist

--- frontend/src/components/ProviderComposer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ProviderComposer = ({ contexts, children }) => {
-  return contexts.reduceRight((acc, Context) => {
-    return <Context>{acc}</Context>;
-  }, children);
-};
-
-export default ProviderComposer;
